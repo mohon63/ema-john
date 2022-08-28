8c4885592fe43ae9a475ba7770a34718f27f2b08
mohon63 <mohonhossen63@gmail.com>
2022-08-29 01:49:42 +0600

    cart order calculation NaN problem soleved and added _redirects file add

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import useProducts from '../../hooks/useProducts';
+import Cart from '../Cart/Cart';
 import useCart from './../../hooks/useCart';
 
 const Orders = () => {
     const [products, setProducts] = useProducts();
     const [cart, setCart] = useCart(products);
     return (
-        <div>
-            <h2>This is orders page :{products.length}</h2>
-            <p>Cart has :{cart.length}</p>
+        <div className="shop-container">
+            <div className="products-container">
+
+            </div>
+            <div className="cart-container">
+                <Cart cart={cart}></Cart>
+            </div>
         </div>
     );
 };
