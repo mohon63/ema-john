baf9eca792d5031210000dad73cf762c4f40d225
mohon63 <mohonhossen63@gmail.com>
2022-08-28 19:36:13 +0600

    useCart , useing hooks in order page

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import useProducts from '../../hooks/useProducts';
+import useCart from './../../hooks/useCart';
 
 const Orders = () => {
     const [products, setProducts] = useProducts();
+    const [cart, setCart] = useCart(products);
     return (
         <div>
             <h2>This is orders page :{products.length}</h2>
+            <p>Cart has :{cart.length}</p>
         </div>
     );
 };
