d9c4392ca2d5f19291cc00ed9ba0920b0383ab32
mohon63 <mohonhossen63@gmail.com>
2022-08-30 21:45:58 +0600

    added componets

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import useProducts from '../../hooks/useProducts';
 import Cart from '../Cart/Cart';
+import ReviewItem from '../ReviewItem/ReviewItem';
 import useCart from './../../hooks/useCart';
 
 const Orders = () => {
@@ -8,8 +9,13 @@ const Orders = () => {
     const [cart, setCart] = useCart(products);
     return (
         <div className="shop-container">
-            <div className="products-container">
-
+            <div className="review-item-container">
+                {
+                    cart.map(product => <ReviewItem
+                        product={product}
+                        key={product.id}
+                    ></ReviewItem>)
+                }
             </div>
             <div className="cart-container">
                 <Cart cart={cart}></Cart>
---
+++ src/components/ReviewItem/ReviewItem.js
@@ -0,0 +1,30 @@
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+import './ReviewItem.css'
+
+const ReviewItem = (props) => {
+    const { name, price, shipping, quantity, img } = props.product;
+    return (
+        <div className='review-item'>
+            <div>
+                <img src={img} alt="" />
+            </div>
+            <div className="review-item-details-container">
+                <div className="review-item-details" title={name}>
+                    <p>{name.length > 20 ? name.slice(0, 20) + '...' : name}</p>
+                    <p>Price: <span className='orange-color'>{price}</span></p>
+                    <p><small>Shipping: {shipping}</small></p>
+                    <p><small>Quantity: {quantity}</small></p>
+                </div>
+                <div className="delete-container">
+                    <button className='delete-button'>
+                        <FontAwesomeIcon className='delete-icon' icon={faTrashAlt} />
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ReviewItem;
